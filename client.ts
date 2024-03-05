da8d44f12a1bf3d56168274c9e7418e92586e382
vincent-the-gamer <1127620453@qq.com>
2024-03-06 00:18:05 +0800

    feat: allow https self-signed certificate.

--- client.ts
+++ client.ts
@@ -1,12 +1,20 @@
 import clipboard from "clipboardy"
 import axios from "axios"
 import { CLIENT_TARGET } from "./config"
+import https from "https"
+
+// Allow HTTPS self-signed certificate
+const myAxios = axios.create({
+    httpsAgent: new https.Agent({  
+      rejectUnauthorized: false
+    })
+  });
 
 const currentText = clipboard.readSync()
 
 
 if(currentText) {
-    axios.post(CLIENT_TARGET, {
+    myAxios.post(CLIENT_TARGET, {
         text: currentText
     }).then(res => {
         console.log(res.data.text)
